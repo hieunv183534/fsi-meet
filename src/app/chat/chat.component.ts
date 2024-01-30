661369da5660f920e58a09417821598b10fa8b2c
trungtq <trungtq>
2024-01-30 11:21:48 +0700

    edit ui chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -44,18 +44,37 @@ export class ChatComponent implements OnInit {
 
     this.connection.on("OnMessage", (userId: string, message: string) => {
       this.userInfo = JSON.parse(localStorage.getItem("users") || "[]").find((x: any) => x.id == userId);
-      let msgObj = { sender: this.userInfo, message: message, time: this.getTime(new Date()) };
+      let msgObj = { sender: this.userInfo, message: message, time: new Date(), showA: false };
 
       if (msgObj.sender.id === this.curUser.nameid) {
         msgObj.sender.name = 'Bạn';
       }
       this.messages.push(msgObj)
+      this.messages?.forEach((m, i: number) => {
+        if (i == 0) {
+          m.showA = true;
+        } else {
+          let _m = this.messages[i - 1];
+          if (m.sender?.id == _m.sender?.id) {
+            let diff = Number(new Date(m.time ?? "")) - Number(new Date(_m.time ?? ""));
+            if (diff > 240000) {
+              m.showA = true;
+            } else {
+              return;
+            }
+          } else {
+            m.showA = true;
+          }
+        }
+      });
     });
 
   }
   async send() {
+    if (this.contentText === "")
+      return;
     await this.connection?.invoke("Chat", this.contentText);
-    this.contentText= "";
+    this.contentText = "";
   }
   handleKeyDown(event: KeyboardEvent): void {
     if (event.key === 'Enter' && event.shiftKey) {
@@ -79,7 +98,7 @@ export class ChatComponent implements OnInit {
       event.preventDefault();
     }
   }
-  handleChange(): void {
+  handleChange(event: any): void {
     if (this.contentText === '') {
       this.rows = 1;
     }
